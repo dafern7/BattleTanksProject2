a6b5976e2c8f6ff6aa403314195ff575ede9a546
DESKTOP-72D0P0I\Richard <dafern7@yahoo.com>
2017-08-30 02:54:38 -0400

    Second Copy Battleground with TankCamera

---
+++ BattleTanksProject2/Source/BattleTanksProject2/BattleTanksProject2.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTanksProject2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BattleTanksProject2, "BattleTanksProject2" );
---
+++ BattleTanksProject2/Source/BattleTanksProject2/BattleTanksProject2GameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BattleTanksProject2GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANKSPROJECT2_API ABattleTanksProject2GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
